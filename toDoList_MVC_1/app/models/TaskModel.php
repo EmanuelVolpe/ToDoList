4e24e654de51f22f9c28d6cb29e0ebd0e0ab1d78
EmanuelVolpe <emanuelvolpe@gmail.com>
2021-02-02 23:27:30 -0300

    refactor connect

--- toDoList_MVC_1/app/models/TaskModel.php
+++ toDoList_MVC_1/app/models/TaskModel.php
@@ -2,34 +2,36 @@
 
     class TaskModel {
 
+        private $db;
+
+        function __construct() {
+            $this->db = $this->connect();
+        }
+
         private function connect() {
             $db = new PDO('mysql:host=localhost;' . 'dbname=db_todolist;charset=utf8', 'root', '');
             return $db;
         }
 
         function getTasks() {
-            $db = $this->connect();
-            $query = $db->prepare('SELECT * FROM tareas');
+            $query = $this->db->prepare('SELECT * FROM tareas');
             $query->execute();
             $tasks = $query->fetchAll(PDO::FETCH_OBJ);
             return $tasks;
         }
 
         function insertTask($titulo, $descripcion, $prioridad) {
-            $db = $this->connect();
-            $query = $db->prepare('INSERT INTO tareas (titulo, descripcion, prioridad) VALUES (?,?,?)');
+            $query = $this->db->prepare('INSERT INTO tareas (titulo, descripcion, prioridad) VALUES (?,?,?)');
             $query->execute([$titulo, $descripcion, $prioridad]);
         }
 
         function removeTask($id) {
-            $db = $this->connect();
-            $query = $db->prepare('DELETE FROM tareas WHERE id_tarea=?');
+            $query = $this->db->prepare('DELETE FROM tareas WHERE id_tarea=?');
             $query->execute([$id]);
         }
 
         function finalizeTask($id) {
-            $db = $this->connect();
-            $query = $db->prepare('UPDATE tareas SET finalizada = 1 WHERE id_tarea=?');
+            $query = $this->db->prepare('UPDATE tareas SET finalizada = 1 WHERE id_tarea=?');
             $query->execute([$id]);
         }
     }
